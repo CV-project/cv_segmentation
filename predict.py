3e17536a054cf7223f527cc86276761a4e3da042
Zhang Yong <yongzhang@qq.com>
2018-12-17 17:25:17 +0800

    fix predict

--- predict.py
+++ predict.py
@@ -50,7 +50,7 @@ saver.restore(sess, args.checkpoint_path)
 
 print("Testing image " + args.image)
 
-loaded_image = utils.load_image(args.image)
+loaded_image = utils.load_image_baidu(args.image)
 resized_image =cv2.resize(loaded_image, (args.crop_width, args.crop_height))
 input_image = np.expand_dims(np.float32(resized_image[:args.crop_height, :args.crop_width]),axis=0)/255.0
 
@@ -64,7 +64,7 @@ output_image = helpers.reverse_one_hot(output_image)
 
 out_vis_image = helpers.colour_code_segmentation(output_image, label_values)
 file_name = utils.filepath_to_name(args.image)
-cv2.imwrite("%s_pred.png"%(file_name),cv2.cvtColor(np.uint8(out_vis_image), cv2.COLOR_RGB2BGR))
+cv2.imwrite("%s_pred.png"%(file_name), np.uint8(out_vis_image))
 
 print("")
 print("Finished!")
